7b1bbd80e2d1544f51a2b6fea992b98c24ff4161
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-11-23 10:40:34 +0530

    feat: auth middleware added

---
+++ middleware/authMiddleware.go
@@ -0,0 +1,44 @@
+package middleware
+
+import (
+	"Expense-Tracker-go/utils"
+	"context"
+	"net/http"
+	"strings"
+)
+
+type contextKey string
+
+const userClaimsKey = contextKey("userClaims")
+
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		//Get the authorization header
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header missing", http.StatusUnauthorized)
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		info, err := utils.ValidateToken(tokenString)
+
+		if err != nil {
+			http.Error(w, "Invalid or Expired token", http.StatusUnauthorized)
+			return
+		}
+
+		//add claims to the request context
+		ctx := context.WithValue(r.Context(), userClaimsKey, info)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+
+	})
+}
--- routes/transactionRoutes.go
+++ routes/transactionRoutes.go
@@ -1,16 +1,12 @@
 package routes
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func transactionRoutes(r *gin.RouterGroup) {
-	r.GET("/", getAllTransaction)
-	r.GET("/latest", get4Transaction)
-	r.GET("/one/:id", getTransactionbyId)
-	r.POST("/", addTransaction)
-	r.PUT("/:id", updateTransaction)
-	r.GET("/sumIncome", getSumOfIncomeTransactionsByUserId)
-	r.GET("/sumExpense", getSumOfExpenseTransactionsByUserId)
-	r.DELETE("/:id", deleteTransactions)
-}
+//func transactionRoutes(r *gin.RouterGroup) {
+//	r.GET("/", getAllTransaction)
+//	r.GET("/latest", get4Transaction)
+//	r.GET("/one/:id", getTransactionbyId)
+//	r.POST("/", addTransaction)
+//	r.PUT("/:id", updateTransaction)
+//	r.GET("/sumIncome", getSumOfIncomeTransactionsByUserId)
+//	r.GET("/sumExpense", getSumOfExpenseTransactionsByUserId)
+//	r.DELETE("/:id", deleteTransactions)
+//}
