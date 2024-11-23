3496273ac99ab12774c892c287479f37ceeee205
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-11-23 21:29:33 +0530

    chore: validation added for add transaction function

--- controllers/transactionController.go
+++ controllers/transactionController.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"Expense-Tracker-go/models"
 	"Expense-Tracker-go/services"
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"net/http"
 )
 
@@ -17,17 +19,29 @@ func AddTransaction(c *gin.Context) {
 		return
 	}
 
-	//User, ok := User.(string)
-	//if !ok {
-	//	c.JSON(http.StatusUnauthorized, gin.H{"Error": "Invalid or missing user claims"})
-	//	return
-	//}
-
 	var Transaction models.Transaction
 	Transaction.UserName = User.(string)
 
 	if err := c.ShouldBind(&Transaction); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"Error": "Binding data Error", "Details": err.Error()})
+		return
+	}
+
+	//validating User Input
+
+	validate := validator.New()
+	if err := validate.Struct(Transaction); err != nil {
+		var validationErrors validator.ValidationErrors
+		errors.As(err, &validationErrors)
+		errorMessages := make(map[string]string)
+
+		for _, validationError := range validationErrors {
+			field := validationError.Field() // Field name
+			tag := validationError.Tag()     // Validation rule
+			errorMessages[field] = "Validation failed for " + field + " (" + tag + ")"
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{"ValidationErrors": errorMessages})
 		return
 	}
 
--- models/transactionModel.go
+++ models/transactionModel.go
@@ -8,8 +8,8 @@ const (
 )
 
 type Transaction struct {
-	UserName    string `json:"username"`
-	Type        Type   `json:"type"`
-	Description string `json:"description"`
-	Amount      int    `json:"amount"`
+	UserName    string `json:"username" validate:"required"`            // UserName is required
+	Type        Type   `json:"type" validate:"required,oneof=0 1"`      // Type must be either 0 (Income) or 1 (Expense)
+	Description string `json:"description" validate:"required,max=255"` // Description is required, max length 255 characters
+	Amount      int    `json:"amount" validate:"required,gt=0"`         // Amount is required and must be greater than 0
 }
