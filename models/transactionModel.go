dda3225fd516bd0a9f1f3a4eac12a4cd149678d1
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-11-23 23:10:00 +0530

    chore: object id of the documents added for returning from database

--- models/transactionModel.go
+++ models/transactionModel.go
@@ -1,5 +1,7 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Type int
 
 const (
@@ -8,8 +10,9 @@ const (
 )
 
 type Transaction struct {
-	UserName    string `json:"username" validate:"required"`            // UserName is required
-	Type        Type   `json:"type" validate:"required,oneof=0 1"`      // Type must be either 0 (Income) or 1 (Expense)
-	Description string `json:"description" validate:"required,max=255"` // Description is required, max length 255 characters
-	Amount      int    `json:"amount" validate:"required,gt=0"`         // Amount is required and must be greater than 0
+	ID          primitive.ObjectID `bson:"_id,omitempty"`                           // Maps the MongoDB `_id` field
+	UserName    string             `json:"username" validate:"required"`            // UserName is required
+	Type        Type               `json:"type" validate:"required,oneof=0 1"`      // Type must be either 0 (Income) or 1 (Expense)
+	Description string             `json:"description" validate:"required,max=255"` // Description is required, max length 255 characters
+	Amount      int                `json:"amount" validate:"required,gt=0"`         // Amount is required and must be greater than 0
 }
